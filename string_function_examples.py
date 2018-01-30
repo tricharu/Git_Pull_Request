e6ce6fd189fb1eafdee05d172785b3650158b445
ricky-aug <35448700+ricky-aug@users.noreply.github.com>
2018-01-30 09:27:18 +0000

    Create string_function_examples.py

---
+++ string_function_examples.py
@@ -0,0 +1,10 @@
+'''
+This file describe the String function with example
+'''
+s="how are you"
+
+# return the length of the string
+print(len(s))  #the length is 11
+
+#title() return the string which contains of First character of string in Capital
+print(s.title())  # How are you
