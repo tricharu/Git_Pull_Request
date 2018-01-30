19f85312cbed0fd323bee21e3f3df5e903b4d2d2
tricharu <35517169+tricharu@users.noreply.github.com>
2018-01-30 15:22:52 +0530

    Update string_function_examples.py

--- string_function_examples.py
+++ string_function_examples.py
@@ -8,3 +8,10 @@ print(len(s))  #the length is 11
 
 #title() return the string which contains of First character of string in Capital
 print(s.title())  # How are you
+
+
+# upper() return the string in upper case
+print(s.upper()) #HOW ARE YOU
+
+#isupper return true or false, whether the string is in upper case or not
+print(s.isupper()) #False
